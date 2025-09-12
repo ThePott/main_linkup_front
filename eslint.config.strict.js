9431fc0bc2ff2822e508d076113e0f8f8712bb6a
Tynahan <nusilite@gmail.com>
2025-09-12 10:09:54 +0900

    chore(init): 허스키 설치 및 초기 세팅

---
+++ eslint.config.strict.js
@@ -0,0 +1,9 @@
+import baseConfig from "./eslint.config.js";
+
+export default baseConfig.map((config) => ({
+  ...config,
+  rules: {
+    ...config.rules,
+    "no-console": "error", // Error blocks pushes
+  },
+}));
