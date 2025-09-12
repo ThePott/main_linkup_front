947d7c1c409050e2e1ec374524be975424c6fff1
Tynahan <nusilite@gmail.com>
2025-09-12 18:32:32 +0900

    feat(button): 공통 컴포넌트 버튼 제작

--- src/pages/SuperUserPage.jsx
+++ src/pages/SuperUserPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Modal from "../package/Modal";
+import CustomButton from "../package/CustomButton";
 
 const SuperUserPage = () => {
     const [isOpened, setIsOpened] = useState(true);
@@ -16,6 +17,13 @@ const SuperUserPage = () => {
             <div>빈 칸 채우는 용의 아무 텍스트</div>
             <div>빈 칸 채우는 용의 아무 텍스트</div>
             <div>빈 칸 채우는 용의 아무 텍스트</div>
+            <CustomButton shape="PILL">내용물</CustomButton>
+            <CustomButton shape="RECTANGLE">내용물</CustomButton>
+            <CustomButton isOn={true}>내용물</CustomButton>
+            <CustomButton isOn={true} color="RED">
+                내용물
+            </CustomButton>
+            <CustomButton>내용물</CustomButton>
         </div>
     );
 };
