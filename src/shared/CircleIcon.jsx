fc53ea49b9e845184f619cad34f4b2715723cfa2
heehaa25 <black1berrychoi@gmail.com>
2025-09-15 14:49:09 +0900

    fix(CircleIcon):원형 아이콘 구현 갱신 #31

--- src/shared/CircleIcon.jsx
+++ src/shared/CircleIcon.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import styles from "./CircleIcon.module.css";
+import { useNavigate } from "react-router";
 
-const CircleIcon = ({ artistImg, artistName }) => {
+const CircleIcon = ({ artist }) => {
+  const { id, name, img_face } = artist;
+  const navigate = useNavigate();
+
   return (
-    <>
-      <img className={styles.image} src={artistImg} alt={artistName} />
-    </>
+    <li
+      key={id}
+      className={styles.container}
+      onClick={() => {
+        navigate(`/detail/${id}`);
+        console.log(id);
+      }}
+    >
+      <img className={styles.image} src={img_face} alt={name} />
+    </li>
   );
 };
 
