b4f5148a35abd00d89f14d1ba15ca1bea70b4b24
heehaa25 <black1berrychoi@gmail.com>
2025-09-15 18:17:14 +0900

    fix(detail-route): 상세 경로에 artistId 추가 및 프롭 네이밍 개선

--- src/package/SuggestedCard.jsx
+++ src/package/SuggestedCard.jsx
@@ -3,20 +3,19 @@ import styles from "./SuggestedCard.module.css";
 import { useNavigate } from "react-router";
 import RoundBox from "./RoundBox";
 
-const SuggestedCard = ({ idolImg, idolName, idolId }) => {
+const SuggestedCard = ({ artistImg, artistName, artistId }) => {
   const navigate = useNavigate();
 
-  // detail뒤에 id를 줘야함
   const handleClick = () => {
-    navigate(`/detail`);
-    console.log(idolId);
+    navigate(`/detail${artistId}`);
+    console.log(artistId);
   };
 
   return (
     <RoundBox>
       <img
-        src={idolImg}
-        alt={idolName}
+        src={artistImg}
+        alt={artistName}
         className={styles.image}
         onClick={handleClick}
       />
