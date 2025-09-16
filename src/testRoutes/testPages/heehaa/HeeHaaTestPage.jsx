a3981b5ad7fa2eb1f4add7ff03c59fb35533f014
heehaa25 <black1berrychoi@gmail.com>
2025-09-16 11:08:11 +0900

    fix(roundbox-generalization):라운드 박스 고정 크기 삭제 및 프롭 추가, 전역css 추가

--- src/testRoutes/testPages/heehaa/HeeHaaTestPage.jsx
+++ src/testRoutes/testPages/heehaa/HeeHaaTestPage.jsx
@@ -1,13 +1,17 @@
 import RecommendContent from "../../../features/front/recommend/RecommendContent";
+import RoundBox from "../../../package/RoundBox";
 import mockData from "../../../shared/store/dummyHeehaa.json";
 
 const HeeHaaTestPage = () => {
-  const artists = mockData;
-  return (
-    <ul>
-      <RecommendContent artists={artists} />
-    </ul>
-  );
+    const artists = mockData;
+    return (
+        <ul>
+            <RecommendContent artists={artists} />
+            <RoundBox padding="md" onClick={() => console.log("click")}>
+                hi
+            </RoundBox>
+        </ul>
+    );
 };
 
 export default HeeHaaTestPage;
