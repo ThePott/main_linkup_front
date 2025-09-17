60030cc26b5d5211e6312eda6f34340edf875a57
heehaa25 <black1berrychoi@gmail.com>
2025-09-17 17:52:31 +0900

    feat(schedule): 전체 콘텐츠 페이지에서 달력과 일일 일정을 보여주는 부분을 추가- 우선 스태틱하게 구현

--- src/features/front/total/TotalContent.jsx
+++ src/features/front/total/TotalContent.jsx
@@ -1,21 +1,82 @@
+import Calendar from "../../../package/calendar/Calendar";
+import RoundBox from "../../../package/RoundBox";
+import ArtistCard from "../../../shared/ArtistCard";
 import CircleIcon from "../../../shared/CircleIcon";
 import styles from "./TotalContent.module.css";
 
 const TotalContent = ({ subscribe }) => {
     return (
         <div className={styles.container}>
-            <div className={styles.header}>
-                <div>
-                    {subscribe.map((artist) => (
-                        <CircleIcon
-                            key={artist.id}
-                            artist={artist}
-                            type="torso"
-                            imgWidth={100}
-                            borderRadius="50%"
-                        />
-                    ))}
-                </div>
+            {subscribe.map((artist) => (
+                <CircleIcon
+                    key={artist.id}
+                    artist={artist}
+                    type="torso"
+                    imgWidth={100}
+                    borderRadius="50%"
+                />
+            ))}
+
+            <RoundBox style={{ overflow: "hidden" }}>
+                <ArtistCard type={"banner"} artist={subscribe[0]} />
+            </RoundBox>
+            <p className={styles.text}>스케줄</p>
+            <div className={styles.calendarContainer}>
+                <span
+                    className={styles.calendar}
+                    onClick={(e) => console.log(e.target.value)}
+                >
+                    <Calendar />
+                </span>
+                <span>
+                    <RoundBox
+                        style={{
+                            width: "500px",
+                            height: "100px",
+                            lineHeight: "100px",
+                            marginLeft: "20px",
+                            marginBottom: "10px",
+                        }}
+                    >
+                        <p className={styles.dailySchedule}>
+                            <span className={styles.date}>01월 00일 </span>
+                            <span className={styles.scheduleLocation}>
+                                사직 콘서트
+                            </span>
+                        </p>
+                    </RoundBox>
+                    <RoundBox
+                        style={{
+                            width: "500px",
+                            height: "100px",
+                            lineHeight: "100px",
+                            marginLeft: "20px",
+                            marginBottom: "10px",
+                        }}
+                    >
+                        <p className={styles.dailySchedule}>
+                            <span className={styles.date}>02월 00일 </span>
+                            <span className={styles.scheduleLocation}>
+                                라팍 콘서트
+                            </span>
+                        </p>
+                    </RoundBox>
+                    <RoundBox
+                        style={{
+                            width: "500px",
+                            height: "100px",
+                            lineHeight: "100px",
+                            marginLeft: "20px",
+                        }}
+                    >
+                        <p className={styles.dailySchedule}>
+                            <span className={styles.date}>03월 00일 </span>
+                            <span className={styles.scheduleLocation}>
+                                잠실 콘서트
+                            </span>
+                        </p>
+                    </RoundBox>
+                </span>
             </div>
         </div>
     );
