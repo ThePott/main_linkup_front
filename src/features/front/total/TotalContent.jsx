01d7fe63ba960b58acce9cbca2b0b65b241cf657
heehaa25 <black1berrychoi@gmail.com>
2025-09-17 17:42:01 +0900

    feat(subscribe): 전체 콘텐츠 페이지에서 원형 아이콘 구현

--- src/features/front/total/TotalContent.jsx
+++ src/features/front/total/TotalContent.jsx
@@ -1,5 +1,24 @@
-const TotalContent = () => {
-    return <div>전체 콘텐트</div>;
+import CircleIcon from "../../../shared/CircleIcon";
+import styles from "./TotalContent.module.css";
+
+const TotalContent = ({ subscribe }) => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <div>
+                    {subscribe.map((artist) => (
+                        <CircleIcon
+                            key={artist.id}
+                            artist={artist}
+                            type="torso"
+                            imgWidth={100}
+                            borderRadius="50%"
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default TotalContent;
