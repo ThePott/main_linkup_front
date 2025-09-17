12a6328322bf6f5a6c7743758b64fc6f3be91268
Tynahan <nusilite@gmail.com>
2025-09-18 08:17:04 +0900

    chore(jsdoc): 커스텀 이미지 jsdoc 추가

---
+++ src/package/customImage/CustomImage.jsx
@@ -0,0 +1,13 @@
+/**
+ * Custom image component with shape and height options
+ * @param {Object} props - The component props
+ * @param {string} props.url - The image URL
+ * @param {'CIRCLE' | 'ROUNDED_RECTANGLE' | 'SHARP_RECTANGLE'} props.shape - The shape of the image
+ * @param {'SM' | 'MD' | 'LG'} props.height - The height size of the image
+ * @returns {JSX.Element}
+ */
+const CustomImage = ({ url, shape, height }) => {
+    return <div></div>;
+};
+
+export default CustomImage;
