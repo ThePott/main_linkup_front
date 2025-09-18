c00562d012c66a7bfb85ac326bbab951e24c7289
kim-mijin <mycrimosn@naver.com>
2025-09-18 17:55:22 +0900

    refactor(fanpostcard): 공통컴포넌트 추가적용 및 코드 개선 #92

--- src/shared/FanpostCard.jsx
+++ src/shared/FanpostCard.jsx
@@ -1,36 +1,23 @@
-import GridContainer from "../package/gridContainer/GridContainer.jsx";
-import RoundBox from "../package/RoundBox.jsx";
+import GridCardContainer from "../shared/GridCardContainer/GridCardContainer.jsx";
+import CustomImageCard from "../shared/CustomImageCard/CustomImageCard.jsx";
 
-const FanPostSection = ({ posts, onClickPost, limit = posts.length, cols = 3 }) => {
+const FanPostSection = ({ posts, onClickPost, limit = posts.length, cols = "auto"}) => {
   return (
-    <div>
-      <GridContainer cols={cols}>
-        {posts.slice(0, limit).map((post) => (
-          <div key={post.postId} style={{ cursor: "pointer" }}>
-
-            <RoundBox
-              onClick={() => onClickPost(post.postId)}
-              style={{ height: "160px" }} 
-            >
-            </RoundBox>
-
-            <div
-              style={{
-                display: "flex",
-                gap: "0.9rem",
-                marginTop: "0.6rem",
-                marginLeft: "0.6rem",
-                fontSize: "1rem",
-              }}
-            >
-              {/* 좋아요수, 댓글수는 임의로 설정 */}
-              <span>♥ {post.likes ?? 55}</span>
-              <span>💬 {post.comments ?? 13}</span>
-            </div>
+    <GridCardContainer cols={cols}>
+      {posts.slice(0, limit).map((post) => (
+        <div key={post.postId} onClick={() => onClickPost(post.postId)}>
+          <CustomImageCard
+            url={post.imgUrl}
+            alt={post.title}
+          />
+          {/* 댓글수와 좋아요수는 임의로 작성함  */}
+          <div>
+            <span>♥ {post.likes ?? 55}</span>
+            <span>💬 {post.comments ?? 13}</span>
           </div>
-        ))}
-      </GridContainer>
-    </div>
+        </div>
+      ))}
+    </GridCardContainer>
   );
 };
 
