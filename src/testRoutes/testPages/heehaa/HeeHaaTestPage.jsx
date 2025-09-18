8b5fb8621785b0c248390abcc3db01b68f149990
heehaa25 <black1berrychoi@gmail.com>
2025-09-18 10:53:32 +0900

    feat(fullpagetest):전체 콘텐츠를 스테틱하게 구현한 테스트 페이지

--- src/testRoutes/testPages/heehaa/HeeHaaTestPage.jsx
+++ src/testRoutes/testPages/heehaa/HeeHaaTestPage.jsx
@@ -1,27 +1,29 @@
 import RecommendContent from "../../../features/front/recommend/RecommendContent";
+import TotalContent from "../../../features/front/total/TotalContent";
 import ArtistCard from "../../../shared/ArtistCard";
 import CircleIcon from "../../../shared/CircleIcon";
 import mockData from "../../../shared/store/dummyHeehaa.json";
 
 const HeeHaaTestPage = () => {
-    const artists = mockData;
+    const subscribe = mockData;
     return (
-        <ul>
-            <RecommendContent artists={artists} type="torso" imgWidth={200} />
-            <RecommendContent artists={artists} type="face" />
-            {artists.map((artist) => (
-                <>
-                    <CircleIcon
-                        key={artist.id}
-                        artist={artist}
-                        type="face"
-                        imgWidth={150}
-                        borderRadius="50%"
-                    />
-                    <ArtistCard artist={artist} type="banner" />
-                </>
-            ))}
-        </ul>
+        <TotalContent subscribe={subscribe} />
+        // <ul>
+        //     <RecommendContent artists={artists} type="torso" imgWidth={200} />
+        //     <RecommendContent artists={artists} type="face" />
+        //     {artists.map((artist) => (
+        //         <>
+        //             <CircleIcon
+        //                 key={artist.id}
+        //                 artist={artist}
+        //                 type="face"
+        //                 imgWidth={150}
+        //                 borderRadius="50%"
+        //             />
+        //             <ArtistCard artist={artist} type="banner" />
+        //         </>
+        //     ))}
+        // </ul>
     );
 };
 
