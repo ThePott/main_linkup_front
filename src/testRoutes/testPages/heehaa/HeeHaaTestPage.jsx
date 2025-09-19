d9fdcecaf5a283778af72c451f8c5c42c0ffce6d
heehaa25 <black1berrychoi@gmail.com>
2025-09-19 18:48:10 +0900

    chore(testpage): 관련 사항 확인 테스트 페이지#52

--- src/testRoutes/testPages/heehaa/HeeHaaTestPage.jsx
+++ src/testRoutes/testPages/heehaa/HeeHaaTestPage.jsx
@@ -1,25 +1,12 @@
-import RoundBox from "../../../package/RoundBox";
-import mockData from "../../../shared/store/dummyHeehaa.json";
+import RecommendContent from "../../../features/front/recommend/RecommendContent";
+import TotalContent from "../../../features/front/total/TotalContent";
 import styles from "./HeeHaaTestPage.module.css";
 
 const HeeHaaTestPage = () => {
-    const subscribe = mockData;
     return (
         <>
-            <RoundBox className={styles.container}>
-                <img
-                    src="https://plus.unsplash.com/premium_photo-1693011410791-98015d7021e8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8"
-                    alt="img"
-                    style={{ width: "50px" }}
-                />
-            </RoundBox>
-            <RoundBox>
-                <img
-                    src="https://plus.unsplash.com/premium_photo-1693011410791-98015d7021e8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8"
-                    alt="img"
-                    style={{ width: "50px" }}
-                />
-            </RoundBox>
+            {/* <RecommendContent /> */}
+            <TotalContent />
         </>
     );
 };
