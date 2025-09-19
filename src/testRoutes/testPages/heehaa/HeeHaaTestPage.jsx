f564f2d5c1ae67ae6df461ed2e3d1f6c857f04f2
heehaa25 <black1berrychoi@gmail.com>
2025-09-19 10:41:45 +0900

    chore(dummy):팬 포스팅에 필요한 예상 더미 데이터와 테스트 페이지

--- src/testRoutes/testPages/heehaa/HeeHaaTestPage.jsx
+++ src/testRoutes/testPages/heehaa/HeeHaaTestPage.jsx
@@ -1,27 +1,25 @@
-import RecommendContent from "../../../features/front/recommend/RecommendContent";
-import ArtistCard from "../../../shared/ArtistCard";
-import CircleIcon from "../../../shared/CircleIcon";
+import { useState } from "react";
+import MypageContent from "../../../features/mypage/MypageContent";
 import mockData from "../../../shared/store/dummyHeehaa.json";
+import mockSubData from "../../../shared/store/dummy2Heehaa.json";
 
 const HeeHaaTestPage = () => {
-    const artists = mockData;
+    const [fanPosting, setFanPosting] = useState(mockData);
+    const subscribeArray = mockSubData;
+
+    const [isOpen, setIsOpen] = useState(false);
+    const handleCreate = (data) => {
+        console.log(data);
+    };
+
     return (
-        <ul>
-            <RecommendContent artists={artists} type="torso" imgWidth={200} />
-            <RecommendContent artists={artists} type="face" />
-            {artists.map((artist) => (
-                <>
-                    <CircleIcon
-                        key={artist.id}
-                        artist={artist}
-                        type="face"
-                        imgWidth={150}
-                        borderRadius="50%"
-                    />
-                    <ArtistCard artist={artist} type="banner" />
-                </>
-            ))}
-        </ul>
+        <>
+            <MypageContent
+                post={fanPosting}
+                subscribeArray={subscribeArray}
+                onSubmit={handleCreate}
+            />
+        </>
     );
 };
 
