a6a9ee2102076ee6217f42f00665ee0125bf1e55
heehaa25 <black1berrychoi@gmail.com>
2025-09-22 17:59:48 +0900

    feat(main): 팬 포스트 경로 추가#124

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import LoginPage from "./pages/LoginPage";
 import queryClient from "./shared/services/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import Layout from "./pages/Layout";
+import FanPostWritePage from "./features/mypage/FanPostWritePage";
 
 const ProductRouteArray = [
     {
@@ -21,6 +22,7 @@ const ProductRouteArray = [
             { path: "/", element: <FrontPage /> },
             { path: "/detail/:type/:id", element: <DetailPage /> },
             { path: "/mypage", element: <MyPage /> },
+            { path: "/mypage/write", element: <FanPostWritePage /> },
             { path: "/agency", element: <AgencyPage /> },
             { path: "/signup", element: <SignupPage /> },
             { path: "/login", element: <LoginPage /> },
@@ -39,5 +41,5 @@ const router = createBrowserRouter(routeArray);
 createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
-    </QueryClientProvider>,
+    </QueryClientProvider>
 );
