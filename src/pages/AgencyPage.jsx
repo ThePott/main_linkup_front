d507496163997ddfd6f1081b3bcc4e01fe24f4fc
Tynahan <nusilite@gmail.com>
2025-09-22 17:30:37 +0900

    chore(dead): 더미 데이터 호출 부분 삭제 #138

--- src/pages/AgencyPage.jsx
+++ src/pages/AgencyPage.jsx
@@ -1,17 +1,8 @@
-import { useEffect } from "react";
 import AgencyContent from "../features/agency/AgencyContent";
-import { dummyAgencyUser } from "../shared/store/dummyThepott.js";
-import useLinkUpStore from "../shared/store/store";
 import AgencySkeleton from "../features/agency/AgencySkeleton";
 
 const AgencyPage = () => {
     const isLoading = false;
-    const setUser = useLinkUpStore((state) => state.setUser);
-
-    useEffect(() => {
-        setUser(dummyAgencyUser);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
 
     return (
         <>
