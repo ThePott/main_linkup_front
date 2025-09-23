b418383ed6550a5f2bcb670d2a83f41baf0979ce
Tynahan <nusilite@gmail.com>
2025-09-23 09:56:57 +0900

    feat(calendar): 달력 모달에 이벤트 정보 띄움 #144

--- src/features/agency/agencyComponents/AgencyCalendarModal.jsx
+++ src/features/agency/agencyComponents/AgencyCalendarModal.jsx
@@ -1,19 +1,54 @@
+import CustomButton from "../../../package/customButton/CustomButton";
+import CustomInputLabeled from "../../../package/CustomInputLabeled";
+import { Vstack } from "../../../package/layout";
 import Modal from "../../../package/modal/Modal";
 import useLinkUpStore from "../../../shared/store/store";
 
 const AgencyCalendarModal = () => {
     const modalKey = useLinkUpStore((state) => state.modalKey);
     const setModalKey = useLinkUpStore((state) => state.setModalKey);
+    const selectedEvent = useLinkUpStore((state) => state.selectedEvent);
+
     const handleDismiss = () => {
         setModalKey(null);
     };
-    console.log({ modalKey });
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+
     return (
         <Modal
             isOn={modalKey === "agencyCalendar"}
             onBackgroundClick={handleDismiss}
         >
-            <p>여기에 아무거나 적기</p>
+            <form onSubmit={handleSubmit}>
+                <Vstack>
+                    <CustomInputLabeled
+                        label="제목"
+                        inputProps={{ defaultValue: selectedEvent?.title }}
+                    />
+                    <CustomInputLabeled
+                        label="설명"
+                        inputProps={{
+                            defaultValue: selectedEvent?.description,
+                        }}
+                    />
+                    <CustomInputLabeled
+                        label="시작 시각"
+                        inputProps={{ defaultValue: selectedEvent?.start_time }}
+                    />
+                    <CustomInputLabeled
+                        label="종료 시각"
+                        inputProps={{ defaultValue: selectedEvent?.end_time }}
+                    />
+                    <CustomInputLabeled
+                        label="장소"
+                        inputProps={{ defaultValue: selectedEvent?.location }}
+                    />
+                    <CustomButton>제출</CustomButton>
+                </Vstack>
+            </form>
         </Modal>
     );
 };
