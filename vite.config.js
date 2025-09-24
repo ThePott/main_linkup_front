f72ee6971582223435bb0185e622fc2c122abde2
paul8729621 <paul872@naver.com>
2025-09-24 16:22:13 +0900

    fix(feature) vite.config.js 프록시 제거 및 원상 복구 #157

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        proxy: {
-            "/api": {
-                target: "http://3.35.210.2:8000", // 백엔드 서버 주소
-                changeOrigin: true,
-            },
-        },
-    },
 });
