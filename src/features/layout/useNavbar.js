aad15622b49f9df0917e7da0fe94b65213144d7a
Tynahan <nusilite@gmail.com>
2025-09-25 15:48:18 +0900

    fix(logout): 다른 페이지에서도 로그아웃 되게 수정 #200

--- src/features/layout/useNavbar.js
+++ src/features/layout/useNavbar.js
@@ -5,6 +5,8 @@ import useLinkUpStore from "../../shared/store/store";
 const useLogoutAndClearCache = () => {
     const access_token = useLinkUpStore((state) => state.access_token);
     const setAccessToken = useLinkUpStore((state) => state.setAccessToken);
+    const setUser = useLinkUpStore((state) => state.setUser);
+    const setArtistArray = useLinkUpStore((state) => state.setArtistArray);
 
     const logout = useCallback(() => {
         if (!access_token) {
@@ -14,6 +16,8 @@ const useLogoutAndClearCache = () => {
         queryClient.removeQueries({ queryKey: ["/api/subscriptions", access_token] }); // Removes all variants
 
         setAccessToken(null);
+        setUser(null);
+        setArtistArray([]);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [access_token]);
 
