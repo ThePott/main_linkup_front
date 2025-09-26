50ab8ad0e1dcabf929907164e11394133abf5e5a
heehaa25 <black1berrychoi@gmail.com>
2025-09-26 20:42:02 +0900

    chore(test): 테스트페이지

--- src/testRoutes/testPages/heehaa/HeeHaaTestPage.jsx
+++ src/testRoutes/testPages/heehaa/HeeHaaTestPage.jsx
@@ -1,3 +1,4 @@
+import TotalContent from "../../../features/front/total/TotalContent";
 import FanPostWritePage from "../../../features/mypage/FanPostWritePage";
 import MyFanPost from "../../../features/mypage/MyFanPost";
 import MyFanPostModal from "../../../features/mypage/MyFanPostModal";
@@ -5,9 +6,10 @@ import MyFanPostModal from "../../../features/mypage/MyFanPostModal";
 const HeeHaaTestPage = () => {
     return (
         <>
-            <MyFanPostModal isOn={true} onClose={false} />
+            <TotalContent />
         </>
     );
 };
 
 export default HeeHaaTestPage;
+//  <MyFanPostModal isOn={true} onClose={false} />
