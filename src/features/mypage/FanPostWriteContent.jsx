3b401fbed2ca70b8d9628bcfd8f27451ad40cbbe
heehaa25 <black1berrychoi@gmail.com>
2025-09-26 22:23:46 +0900

    refactor(fanpostwritecontent): api post 연결

--- src/features/mypage/FanPostWriteContent.jsx
+++ src/features/mypage/FanPostWriteContent.jsx
@@ -4,9 +4,9 @@ import useLinkUpStore from "../../shared/store/store";
 import FileInput from "../../package/FileInput";
 import CustomButton from "../../package/customButton/CustomButton";
 import Modal from "../../package/modal/Modal";
-import axiosInstance from "../../shared/services/axiosInstance";
 import { useMutation } from "@tanstack/react-query";
 import styles from "./FanPostWriteContent.module.css";
+import { axiosReturnsData } from "../../shared/services/axiosInstance";
 
 const FanPostWriteContent = () => {
     const addFanPost = useLinkUpStore((state) => state.addFanPost);
@@ -22,27 +22,28 @@ const FanPostWriteContent = () => {
     const navigate = useNavigate();
 
     const postFanPost = async (formData) => {
-        const response = await axiosInstance.post("/api/posts", formData);
-        console.log("response.data", response);
-        return response;
+        const endpoint = "api/posts";
+        const data = await axiosReturnsData("POST", endpoint, formData);
+        console.log("post fan post data", data);
     };
 
     const mutation = useMutation({
         mutationFn: postFanPost,
-        onSuccess: (newPost) => {
-            addFanPost(newPost);
-            setCompletedModalOpen(true);
-            setTimeout(() => {
-                setCompletedModalOpen(false);
-                navigate("/mypage");
-            }, 2000);
-        },
+        // onSuccess: (newPost) => {
+        //     debugger;
+        //     addFanPost(newPost);
+        //     setCompletedModalOpen(true);
+        //     setTimeout(() => {
+        //         setCompletedModalOpen(false);
+        //         navigate("/mypage");
+        //     }, 2000);
+        // },
     });
 
     const handleSubmit = (event) => {
         event.preventDefault();
         const formData = new FormData(event.target);
-        const imageFile = formData.get("image_url");
+        const imageFile = formData.get("post_image");
         if (!imageFile || (imageFile.size !== undefined && imageFile.size === 0)) {
             setShowImageModal(true);
             return;
@@ -56,6 +57,7 @@ const FanPostWriteContent = () => {
         if (!form) return;
         const formData = new FormData(form);
         mutation.mutate(formData);
+        console.log("mutate", formData);
     };
 
     const handleExit = () => setExitConfirmModalOpen(true);
@@ -84,7 +86,7 @@ const FanPostWriteContent = () => {
         <>
             <form className={styles.form} onSubmit={handleSubmit}>
                 <div className={styles.fileContainer}>
-                    <FileInput className={styles.inputBox} name="image_url" />
+                    <FileInput className={styles.inputBox} name="post_image" />
                     <CustomButton
                         className={`${styles.btn} ${styles.exitBtn}`}
                         onClick={handleExit}
@@ -117,44 +119,33 @@ const FanPostWriteContent = () => {
                 </div>
             </form>
 
-            {exitConfirmModalOpen && (
-                <Modal isOn={exitConfirmModalOpen} onBackgroundClick={handleExitConfirmNo}>
-                    <p className={styles.exitText}>
-                        정말 페이지를 벗어나시겠습니까?
-                        <br /> 내용이 저장되지 않습니다.
-                    </p>
-                    <div className={styles.exitModalBtn}>
-                        <CustomButton onClick={handleExitConfirmYes}>네</CustomButton>
-                        <CustomButton onClick={handleExitConfirmNo}>아니오</CustomButton>
-                    </div>
-                </Modal>
-            )}
+            <Modal isOn={exitConfirmModalOpen} onBackgroundClick={handleExitConfirmNo}>
+                <p className={styles.exitText}>
+                    정말 페이지를 벗어나시겠습니까?
+                    <br /> 내용이 저장되지 않습니다.
+                </p>
+                <div className={styles.exitModalBtn}>
+                    <CustomButton onClick={handleExitConfirmYes}>네</CustomButton>
+                    <CustomButton onClick={handleExitConfirmNo}>아니오</CustomButton>
+                </div>
+            </Modal>
 
-            {showImageModal && (
-                <Modal isOn={showImageModal} onBackgroundClick={handleCloseImageModal}>
-                    <p>이미지를 첨부해주세요.</p>
-                    <CustomButton onClick={handleCloseImageModal}>닫기</CustomButton>
-                </Modal>
-            )}
+            <Modal isOn={showImageModal} onBackgroundClick={handleCloseImageModal}>
+                <p>이미지를 첨부해주세요.</p>
+                <CustomButton onClick={handleCloseImageModal}>닫기</CustomButton>
+            </Modal>
 
-            {confirmModalOpen && (
-                <Modal isOn={confirmModalOpen} onBackgroundClick={handleCancelConfirm}>
-                    <p>정말 출간하시겠습니까?</p>
-                    <div className={styles.confirmModalBtn}>
-                        <CustomButton onClick={handleConfirm}>확인</CustomButton>
-                        <CustomButton onClick={handleCancelConfirm}>아니오</CustomButton>
-                    </div>
-                </Modal>
-            )}
+            <Modal isOn={confirmModalOpen} onBackgroundClick={handleCancelConfirm}>
+                <p>정말 출간하시겠습니까?</p>
+                <div className={styles.confirmModalBtn}>
+                    <CustomButton onClick={handleConfirm}>확인</CustomButton>
+                    <CustomButton onClick={handleCancelConfirm}>아니오</CustomButton>
+                </div>
+            </Modal>
 
-            {completedModalOpen && (
-                <Modal
-                    isOn={completedModalOpen}
-                    onBackgroundClick={() => setCompletedModalOpen(false)}
-                >
-                    <p>출간되었습니다.</p>
-                </Modal>
-            )}
+            <Modal isOn={completedModalOpen} onBackgroundClick={() => setCompletedModalOpen(false)}>
+                <p>출간되었습니다.</p>
+            </Modal>
         </>
     );
 };
