836430298d1055b86d0c7fea20aa80ea2f61e398
Tynahan <134694901+ThePott@users.noreply.github.com>
2025-09-29 07:59:01 +0900

    tempF

--- src/testRoutes/testPages/thepott/ThePottFormTestPage.jsx
+++ src/testRoutes/testPages/thepott/ThePottFormTestPage.jsx
@@ -2,17 +2,20 @@ import { useForm } from "react-hook-form";
 import CustomInput from "../../../package/CustomInput";
 import CustomButton from "../../../package/customButton/CustomButton";
 import LabelGroup from "../../../package/labelGroup/LabelGroup";
+import { inputNameToValidationProps } from "../../../shared/utils/validation";
 
 const TutorialForm = () => {
     const {
         register,
         handleSubmit,
-        watch,
         formState: { errors },
     } = useForm();
-    const onSubmit = (data) => console.log(data);
-
-    console.log(watch("example")); // watch input value by passing the name of it
+    const onSubmit = (data) => {
+        console.log({ data });
+        if (errors) {
+            console.log({ errors });
+        }
+    };
 
     return (
         /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
@@ -20,7 +23,7 @@ const TutorialForm = () => {
             {/* register your input into the hook by invoking the "register" function */}
             <LabelGroup>
                 <LabelGroup.BigLabel>이메일</LabelGroup.BigLabel>
-                <CustomInput {...register("email")} />
+                <CustomInput {...register("email", { type: "email", minLength: 100 })} />
                 {errors.email && (
                     <LabelGroup.SmallLabel>This field is required</LabelGroup.SmallLabel>
                 )}
@@ -28,28 +31,28 @@ const TutorialForm = () => {
             <LabelGroup>
                 <LabelGroup.BigLabel>인증 번호</LabelGroup.BigLabel>
                 <CustomInput {...register("verification_code")} />
-                {errors.email && (
+                {errors.verification_code && (
                     <LabelGroup.SmallLabel>This field is required</LabelGroup.SmallLabel>
                 )}
             </LabelGroup>
             <LabelGroup>
                 <LabelGroup.BigLabel>비밀번호</LabelGroup.BigLabel>
                 <CustomInput {...register("password")} />
-                {errors.email && (
+                {errors.password && (
                     <LabelGroup.SmallLabel>This field is required</LabelGroup.SmallLabel>
                 )}
             </LabelGroup>
             <LabelGroup>
                 <LabelGroup.BigLabel>비밀번호 확인</LabelGroup.BigLabel>
                 <CustomInput {...register("passwordConfirm")} />
-                {errors.email && (
+                {errors.passwordConfirm && (
                     <LabelGroup.SmallLabel>This field is required</LabelGroup.SmallLabel>
                 )}
             </LabelGroup>
             <LabelGroup>
                 <LabelGroup.BigLabel>닉네임</LabelGroup.BigLabel>
                 <CustomInput {...register("nickname")} />
-                {errors.email && (
+                {errors.nickname && (
                     <LabelGroup.SmallLabel>This field is required</LabelGroup.SmallLabel>
                 )}
             </LabelGroup>
