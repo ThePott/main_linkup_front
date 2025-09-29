8bd5d6826a88d4c9bd0e2c9ca2500ab2163966a1
Tynahan <nusilite@gmail.com>
2025-09-29 13:13:56 +0900

    feat(tanstack): 테스트 페이지에서 작동 성공 #236

--- src/shared/services/useSubscriptions.js
+++ src/shared/services/useSubscriptions.js
@@ -24,7 +24,6 @@ const useSubscriptionsQuery = () => {
         if (!data) {
             return;
         }
-        debugger;
         setArtistArray(data);
     }, [data]);
     return { isPendingSubscriptions, errorSubscriptions };
@@ -32,28 +31,44 @@ const useSubscriptionsQuery = () => {
 
 const useSubscriptionsMutate = () => {
     const endpoint = "/api/subscriptions";
+
+    /** 사용법
+     * const { postMutation } = useSubscriptions()
+     * ...
+     * // body = {artist_id: 1}
+     * // newOne = {artist_id:1, group_name: "에스파", stage_name: "카리나"}
+     * ...
+     * <button onClick={ () => postMutation.mutate({ body, newOne })}>구독하기</button>
+     */
     const postMutation = useMutation({
-        mutationFn: (body) => axiosReturnsData("POST", endpoint, body),
-        onMutate: async (body) => {
+        mutationFn: ({ body }) => axiosReturnsData("POST", endpoint, body),
+        onMutate: async ({ newOne }) => {
             await queryClient.cancelQueries({
                 queryKey: [endpoint],
             });
-            const previous = queryClient.getQueryData([queryEndpoint]);
-            const newOne = conversionFn ? conversionFn(body) : newOne;
-            queryClient.setQueryData([queryEndpoint], [...previous, newOne]);
+            const previous = queryClient.getQueryData([endpoint]);
+            // TODO: subscribedAt이 기록되어서 이걸로 sorting이 되면 좋겠더
+            // const newArray = [...previous, newOne].sort((a, b) => a.id - b.id);
+            const newArray = [...previous, newOne];
+            queryClient.setQueryData([endpoint], newArray);
             return { previous };
         },
         onError: async (error, _data, context) => {
             console.error(error);
-            queryClient.setQueryData([queryEndpoint], context.previous);
+            queryClient.setQueryData([endpoint], context.previous);
         },
         onSettled: () => {
             queryClient.invalidateQueries({
-                queryKey: [queryEndpoint],
+                queryKey: [endpoint],
             });
         },
     });
 
+    /** 사용법
+     * const { deleteMutation } = useSubscriptions()
+     * ...
+     * <button onClick={ () => deleteMutation.mutate(artist_id)}>구독 취소하기</button>
+     */
     const deleteMutation = useMutation({
         mutationFn: (id) => axiosReturnsData("DELETE", `${endpoint}/${id}`),
         onMutate: async (id) => {
