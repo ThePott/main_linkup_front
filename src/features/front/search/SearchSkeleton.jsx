19a1de434f5a81fec83e8adda5b9424c5ab24b82
Tynahan <nusilite@gmail.com>
2025-09-30 03:21:21 +0900

    feat(search): 이벤트 목록을 화면에 띄움 #241

---
+++ src/features/front/search/SearchSkeleton.jsx
@@ -0,0 +1,13 @@
+import { Vstack } from "../../../package/layout";
+import Skeleton from "../../../package/skeleton/Skeleton";
+
+const SearchSkeleton = () => {
+    return (
+        <Vstack>
+            Search Skeleton
+            <Skeleton />
+        </Vstack>
+    );
+};
+
+export default SearchSkeleton;
