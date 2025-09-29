9d4d14afeb84d87df7b32b9695ba61a5a4c46bdb
Tynahan <nusilite@gmail.com>
2025-09-29 13:13:56 +0900

    feat(tanstack): 테스트 페이지에서 작동 성공 #236

--- src/testRoutes/testPages/thepott/ThePottTanstackTestPage.jsx
+++ src/testRoutes/testPages/thepott/ThePottTanstackTestPage.jsx
@@ -1,8 +1,52 @@
+import { useEffect, useState } from "react";
+import { getThenLog } from "../../../package/commonServices/fetchVariants";
+import CustomButton from "../../../package/customButton/CustomButton";
+import { Vstack } from "../../../package/layout";
 import useSubscriptions from "../../../shared/services/useSubscriptions";
+import useLinkUpStore from "../../../shared/store/store";
 
 const ThePottTanstackTestPage = () => {
-    useSubscriptions();
-    return <div>Tan Stack Query Test Page</div>;
+    const artistArray = useLinkUpStore((state) => state.artistArray);
+    const { postMutation, deleteMutation } = useSubscriptions();
+    const [idolArray, setIdolArray] = useState([]);
+
+    const baseURL = import.meta.env.VITE_BASE_URL;
+    const access_token = useLinkUpStore((state) => state.access_token);
+    const callback = (data) => {
+        console.log({ data });
+        setIdolArray(data.artists);
+    };
+
+    const firstIdol = idolArray.find((idol) => idol.id === 1);
+
+    const variables = {
+        body: { artist_id: 1 },
+        newOne: { artist_id: 1, stage_name: firstIdol.name, group_name: firstIdol.name },
+    };
+
+    useEffect(() => {
+        getThenLog(`${baseURL}/api/idol`, callback, access_token);
+    }, []);
+
+    return (
+        <Vstack>
+            <p>Tan Stack Query Test Page</p>
+            <p>idol array</p>
+            {idolArray.map((idol) => (
+                <p>{idol.name}</p>
+            ))}
+            <p>subscribing artist array</p>
+            {artistArray.map((artist) => (
+                <p>
+                    {artist.stage_name || artist.group_name}__{artist.id}
+                </p>
+            ))}
+            <CustomButton onClick={() => deleteMutation.mutate(1)}>unsubscribe idol 1</CustomButton>
+            <CustomButton onClick={() => postMutation.mutate(variables)}>
+                subscribe idol 1
+            </CustomButton>
+        </Vstack>
+    );
 };
 
 export default ThePottTanstackTestPage;
