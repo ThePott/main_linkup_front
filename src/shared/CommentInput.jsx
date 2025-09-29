662f8e0fdbab8ab3e9704d8b9a69899f2b79dd67
heehaa25 <black1berrychoi@gmail.com>
2025-09-29 20:17:52 +0900

    refactor(comment):api post에 맞춰 리팩토링#239

--- src/shared/CommentInput.jsx
+++ src/shared/CommentInput.jsx
@@ -2,26 +2,32 @@ import { React, useRef, useState } from "react";
 import styles from "./CommentInput.module.css";
 import CustomInput from "../package/CustomInput";
 import CustomButton from "../package/customButton/CustomButton";
+import useLinkUpStore from "./store/store";
+import { axiosReturnsData } from "./services/axiosInstance";
 
-const CommentInput = ({ onAdd }) => {
+const CommentInput = () => {
+    const selectedFanPost = useLinkUpStore((state) => state.selectedFanPost);
+    const addComment = useLinkUpStore((state) => state.addComment);
+    const post_id = selectedFanPost?.id;
     const [inputText, setInputText] = useState("");
     const isAdding = useRef(false);
 
-    const handleAddComment = () => {
+    const handleAddComment = async () => {
         if (isAdding.current) return;
         if (inputText.trim() === "") return;
-
         isAdding.current = true;
 
         const newComment = {
             id: Date.now(),
-            text: inputText,
-            date: new Date().toLocaleString(),
+            comment_content: inputText,
         };
-        onAdd(newComment);
+        const endpoint = `api/posts/${post_id}/comments`;
+        const savedComment = await axiosReturnsData("POST", endpoint, newComment);
+        addComment(post_id, savedComment);
         setInputText("");
         isAdding.current = false;
     };
+
     return (
         <>
             <form
@@ -32,11 +38,10 @@ const CommentInput = ({ onAdd }) => {
                 }}
             >
                 <CustomInput
-                    name="comment"
+                    name="comment_content"
                     placeholder="댓글을 입력하세요"
                     value={inputText}
-                    onChange={setInputText}
-                    onClick={handleAddComment}
+                    onChange={(e) => setInputText(e.target.value)}
                     className={styles.input}
                 />
 
