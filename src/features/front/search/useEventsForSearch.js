3aef7586987de0f059a96bd18888ebbcba32672c
Tynahan <nusilite@gmail.com>
2025-09-30 13:14:03 +0900

    temp

--- src/features/front/search/useEventsForSearch.js
+++ src/features/front/search/useEventsForSearch.js
@@ -2,13 +2,16 @@ import { useQuery } from "@tanstack/react-query";
 import useLinkUpStore from "../../../shared/store/store";
 import { getThenStoreEventDict } from "./searchApi";
 import { useEffect } from "react";
+import { format } from "date-fns";
 
 const useEventsForSearch = () => {
     const searchResultArray = useLinkUpStore((state) => state.searchResultArray);
     const setEventDict = useLinkUpStore((state) => state.setEventDict);
     const artistIdArray = searchResultArray.map((result) => result.id);
 
-    const endpoint = "/api/events/?artist_id=";
+    const start_date = format(new Date(), "yyyy-MM-dd");
+    const endpoint = `/api/events/?start_date=${start_date}&artist_id=`;
+
     const {
         data,
         isPending: isPendingEvents,
