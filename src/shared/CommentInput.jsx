35d5bb79a884beadba143f16ea2a0abf97f9b5e7
heehaa25 <black1berrychoi@gmail.com>
2025-09-30 14:58:30 +0900

    refactor(comment): 이전 코멘트 get해오기 #253

--- src/shared/CommentInput.jsx
+++ src/shared/CommentInput.jsx
@@ -1,4 +1,4 @@
-import { React, useRef, useState } from "react";
+import { React, useEffect, useRef, useState } from "react";
 import styles from "./CommentInput.module.css";
 import CustomInput from "../package/CustomInput";
 import CustomButton from "../package/customButton/CustomButton";
@@ -7,7 +7,8 @@ import { axiosReturnsData } from "./services/axiosInstance";
 
 const CommentInput = () => {
     const selectedFanPost = useLinkUpStore((state) => state.selectedFanPost);
-    const addComment = useLinkUpStore((state) => state.addComment);
+    const setCommentsByPostId = useLinkUpStore((state) => state.setCommentsByPostId);
+    // const addComment = useLinkUpStore((state) => state.addComment);
     const post_id = selectedFanPost?.id;
     const [inputText, setInputText] = useState("");
     const isAdding = useRef(false);
@@ -17,16 +18,26 @@ const CommentInput = () => {
         if (inputText.trim() === "") return;
         isAdding.current = true;
 
-        const newComment = {
-            id: Date.now(),
-            comment_content: inputText,
-        };
-        const endpoint = `api/posts/${post_id}/comments`;
-        const savedComment = await axiosReturnsData("POST", endpoint, newComment);
-        addComment(post_id, savedComment);
-        setInputText("");
-        isAdding.current = false;
+        const newComment = { comment_content: inputText };
+        try {
+            const endpoint = `api/posts/${post_id}/comments`;
+            await axiosReturnsData("POST", endpoint, newComment);
+            setInputText("");
+        } finally {
+            isAdding.current = false;
+        }
     };
+
+    const getComments = async () => {
+        if (!post_id) return;
+        const endpoint = `api/posts/${post_id}/comments`;
+        const updateComments = await axiosReturnsData("GET", endpoint);
+        setCommentsByPostId(post_id, updateComments);
+    };
+
+    useEffect(() => {
+        getComments();
+    }, []);
 
     return (
         <>
