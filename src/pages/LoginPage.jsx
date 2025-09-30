70db25d76c5544d94857642fc31626240227d4d9
Tynahan <nusilite@gmail.com>
2025-09-30 15:15:22 +0900

    feat(login): 로그인에도 리액트 후크 폼 적용 #261

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,25 +1,38 @@
 import styles from "./LoginPage.module.css";
 import { Link } from "react-router";
 import SocialLoginButton from "../features/login/loginComponents/SocialLoginButton";
-import { loginInputPropsEntryArray } from "../features/login/loginServices/loginInputProps";
 import CustomButton from "../package/customButton/CustomButton";
-import CustomInputLabeled from "../package/CustomInputLabeled";
 import { FullScreen, Hstack, Vstack } from "../package/layout";
 import RoundBox from "../package/RoundBox";
 import useAuth from "../shared/services/useAuth";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+import LabelGroup from "../package/labelGroup/LabelGroup";
+import CustomInput from "../package/CustomInput";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema } from "../shared/validations/zodSchema";
 
 const LoginPage = () => {
     const { errorLogin, isPendingLogin, postLoginMutation } = useAuth();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const { emailHTMLElement, passwordHTMLElement } = event.target;
-        const body = {
-            email: emailHTMLElement.value,
-            password: passwordHTMLElement.value,
-        };
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors },
+    } = useForm({ resolver: zodResolver(loginSchema) });
 
-        postLoginMutation.mutate(body);
+    useEffect(() => {
+        setError("password", {
+            type: "server",
+            message: "이메일 또는 비밀번호가 잘못되었습니다",
+        });
+        console.error();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [errorLogin]);
+
+    const onSubmit = (data) => {
+        postLoginMutation.mutate(data);
     };
 
     return (
@@ -27,24 +40,37 @@ const LoginPage = () => {
             <RoundBox padding="LG">
                 <Vstack gap="xl">
                     <Vstack style={{ width: "400px" }}>
-                        <form onSubmit={handleSubmit}>
+                        <form onSubmit={handleSubmit(onSubmit)}>
                             <Vstack gap="xl">
-                                {loginInputPropsEntryArray.map((entry) => (
-                                    <CustomInputLabeled
-                                        key={entry[0]}
-                                        label={entry[0]}
-                                        inputProps={entry[1]}
-                                    />
-                                ))}
-                                <CustomButton>이메일로 로그인</CustomButton>
+                                <LabelGroup isRed={errors.email}>
+                                    <LabelGroup.BigLabel>이메일</LabelGroup.BigLabel>
+                                    <CustomInput {...register("email")} />
+                                    {errors.email && (
+                                        <LabelGroup.SmallLabel>
+                                            {errors.email.message}
+                                        </LabelGroup.SmallLabel>
+                                    )}
+                                </LabelGroup>
+                                <LabelGroup isRed={errors.password}>
+                                    <LabelGroup.BigLabel>비밀번호</LabelGroup.BigLabel>
+                                    <CustomInput {...register("password")} />
+                                    {errors.password && (
+                                        <LabelGroup.SmallLabel>
+                                            {errors.password.message}
+                                        </LabelGroup.SmallLabel>
+                                    )}
+                                </LabelGroup>
+                                <CustomButton disabled={isPendingLogin}>
+                                    이메일로 로그인
+                                </CustomButton>
                             </Vstack>
                         </form>
                     </Vstack>
 
                     <Vstack>
-                        <SocialLoginButton provider={"GOOGLE"} />
-                        <SocialLoginButton provider={"KAKAO"} />
-                        <SocialLoginButton provider={"FACEBOOK"} />
+                        {/* <SocialLoginButton provider={"GOOGLE"} /> */}
+                        <SocialLoginButton disabled={isPendingLogin} provider={"KAKAO"} />
+                        {/* <SocialLoginButton provider={"FACEBOOK"} /> */}
                     </Vstack>
 
                     <Hstack>
