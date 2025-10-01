12a43a779281286d8289fa9704daa94eb7339659
Tynahan <nusilite@gmail.com>
2025-10-01 16:50:33 +0900

    fix(skeleton): 추천 스켈레톤에서 매핑 키 추가 #285

--- src/features/front/recommend/RecommendContentSkeleton.jsx
+++ src/features/front/recommend/RecommendContentSkeleton.jsx
@@ -1,12 +1,16 @@
-import React from "react";
 import GridContainer from "../../../package/gridContainer/GridContainer";
 import Skeleton from "../../../package/skeleton/Skeleton";
 
 const RecommendContentSkeleton = () => {
     return (
         <GridContainer cols="auto" colMinWidth="200px">
-            {Array.from({ length: 6 }).map((_, idx) => (
-                <Skeleton skeletonVariant="BOX" heightInPixel={200} widthInPixel={180} />
+            {Array.from({ length: 6 }).map((_, index) => (
+                <Skeleton
+                    key={index}
+                    skeletonVariant="BOX"
+                    heightInPixel={200}
+                    widthInPixel={180}
+                />
             ))}
         </GridContainer>
     );
