a769c09c53b5ab28924a485056d6c53b1cbe2a44
Tynahan <nusilite@gmail.com>
2025-10-01 23:30:15 +0900

    feat(tanstack): 댓글 요청하면 UI에 즉각 반영 #292

--- src/shared/CommentInput.jsx
+++ src/shared/CommentInput.jsx
@@ -1,55 +1,47 @@
-import { React, useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import styles from "./CommentInput.module.css";
 import CustomInput from "../package/CustomInput";
 import CustomButton from "../package/customButton/CustomButton";
 import useLinkUpStore from "./store/store";
 import { axiosReturnsData } from "./services/axiosInstance";
+import { useMutation } from "@tanstack/react-query";
+import queryClient from "./services/queryClient";
 
 const CommentInput = () => {
     const selectedFanPost = useLinkUpStore((state) => state.selectedFanPost);
-    const setCommentsByPostId = useLinkUpStore((state) => state.setCommentsByPostId);
-    // const addComment = useLinkUpStore((state) => state.addComment);
     const post_id = selectedFanPost?.id;
     const [inputText, setInputText] = useState("");
-    const isAdding = useRef(false);
 
-    const handleAddComment = async () => {
-        if (isAdding.current) return;
-        if (inputText.trim() === "") return;
-        isAdding.current = true;
+    const endpoint = `/api/posts/${post_id}/comments`;
+    const postMutation = useMutation({
+        mutationFn: ({ body }) => axiosReturnsData("POST", endpoint, body),
+        onMutate: async ({ newOne }) => {
+            await queryClient.cancelQueries({
+                queryKey: [endpoint],
+            });
+            const previous = queryClient.getQueryData([endpoint]);
+            queryClient.setQueryData([endpoint], [...previous, newOne]);
+            return { previous };
+        },
+        onError: (error) => console.error(error),
+    });
 
-        const newComment = { comment_content: inputText };
-        try {
-            const endpoint = `api/posts/${post_id}/comments`;
-            await axiosReturnsData("POST", endpoint, newComment);
-            setInputText("");
-        } finally {
-            isAdding.current = false;
-        }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const { commentHtmlElement } = event.target;
+        const value = commentHtmlElement.value;
+        const body = {
+            comment_content: value,
+        };
+        const newOne = { content: value };
+        postMutation.mutate({ body, newOne });
     };
-
-    // const getComments = async () => {
-    //     if (!post_id) return;
-    //     const endpoint = `api/posts/${post_id}/comments`;
-    //     const updateComments = await axiosReturnsData("GET", endpoint);
-    //     setCommentsByPostId(post_id, updateComments);
-    // };
-
-    // useEffect(() => {
-    //     getComments();
-    // }, []);
 
     return (
         <>
-            <form
-                className={styles.form}
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    handleAddComment();
-                }}
-            >
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <CustomInput
-                    name="comment_content"
+                    name="commentHtmlElement"
                     placeholder="댓글을 입력하세요"
                     value={inputText}
                     onChange={(e) => setInputText(e.target.value)}
