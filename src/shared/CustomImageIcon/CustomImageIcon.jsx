34b43a1b62f994c4a13585431e70924615c26f4c
Tynahan <nusilite@gmail.com>
2025-10-07 14:02:56 +0900

    fix(fan post modal): 팬 포스트 작성자 제대로 보이게 수정 #317

--- src/shared/CustomImageIcon/CustomImageIcon.jsx
+++ src/shared/CustomImageIcon/CustomImageIcon.jsx
@@ -1,7 +1,14 @@
 import CustomImage from "../../package/customImage/CustomImage.jsx";
 
 const CustomImageIcon = ({ url, ...props }) => {
-    return <CustomImage {...props} url={url} height="xs" shape="CIRCLE" />;
+    return (
+        <CustomImage
+            {...props}
+            url={url || import.meta.env.VITE_PLACEHOLDER_IMAGE}
+            height="xs"
+            shape="CIRCLE"
+        />
+    );
 };
 
 export default CustomImageIcon;
