2e0bbac61cf82a53e9f593f72caa224f956398a3
Tynahan <nusilite@gmail.com>
2025-10-07 17:10:08 +0900

    feat(lazy): lazy import로 main.jsx 재구성 #321

--- src/main.jsx
+++ src/main.jsx
@@ -1,42 +1,49 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import FrontPage from "./pages/FrontPage.jsx";
-import DetailPage from "./pages/DetailPage.jsx";
-import MyPage from "./pages/MyPage.jsx";
-import SuperUserPage from "./pages/SuperUserPage.jsx";
-import TestRouteArray from "./testRoutes/";
-import AgencyPage from "./pages/AgencyPage";
-import SignupPage from "./pages/SignupPage.jsx";
-import LoginPage from "./pages/LoginPage";
+import testRouteArray from "./testRoutes/";
 import queryClient from "./shared/services/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import Layout from "./pages/Layout";
 import FanPostWritePage from "./features/mypage/FanPostWritePage";
+import { lazy, Suspense } from "react";
+const FrontPage = lazy(() => import("./pages/FrontPage.jsx"));
+const DetailPage = lazy(() => import("./pages/DetailPage.jsx"));
+const MyPage = lazy(() => import("./pages/MyPage.jsx"));
+const SuperUserPage = lazy(() => import("./pages/SuperUserPage.jsx"));
+const AgencyPage = lazy(() => import("./pages/AgencyPage"));
+const SignupPage = lazy(() => import("./pages/SignupPage.jsx"));
+const LoginPage = lazy(() => import("./pages/LoginPage"));
 
-const ProductRouteArray = [
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { path: "/", element: <FrontPage /> },
-      { path: "/detail/:type/:id", element: <DetailPage /> },
-      { path: "/mypage", element: <MyPage /> },
-      { path: "/mypage/write", element: <FanPostWritePage /> },
-      { path: "/agency", element: <AgencyPage /> },
-      { path: "/signup", element: <SignupPage /> },
-      { path: "/login", element: <LoginPage /> },
-      { path: "/super-user", element: <SuperUserPage /> },
-    ],
-  },
+const productRouteArray = [
+    { path: "/", element: <FrontPage /> },
+    { path: "/detail/:type/:id", element: <DetailPage /> },
+    { path: "/mypage", element: <MyPage /> },
+    { path: "/mypage/write", element: <FanPostWritePage /> },
+    { path: "/agency", element: <AgencyPage /> },
+    { path: "/signup", element: <SignupPage /> },
+    { path: "/login", element: <LoginPage /> },
+    { path: "/super-user", element: <SuperUserPage /> },
 ];
 
-const routeArray = [...ProductRouteArray, ...TestRouteArray];
+const suspenedRouteArray = productRouteArray.map((route) => ({
+    path: route.path,
+    element: <Suspense>{route.element}</Suspense>,
+}));
+
+const routeArray = [
+    ...testRouteArray,
+    {
+        path: "/",
+        element: <Layout />,
+        children: [...productRouteArray, ...suspenedRouteArray],
+    },
+];
 
 const router = createBrowserRouter(routeArray);
 
 createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-  </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+    </QueryClientProvider>,
 );
